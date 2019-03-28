51ea052fad9eedc65b2564fa8e7524265cf1207f
sheikhimtiaz <imticorei54@gmail.com>
2019-03-28 20:27:10 +0600

    update 1.0
    update 1.0

---
+++ LiveChatServer/Helpers/DBConnection.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveChatServer.Helpers
+{
+    public class DBConnection
+    {
+
+        public static async Task EstablishConnection()
+        {
+            string connectionString = "mongodb://172.16.0.13:27017"; 
+            string DBName = "CSharpExamChatServer";
+
+            MongoClient client = new MongoClient(connectionString);
+            IMongoDatabase db = client.GetDatabase(DBName);
+
+            IMongoCollection<BsonDocument> collection = db.GetCollection<BsonDocument>("Users");
+
+            using (IAsyncCursor<BsonDocument> cursor = await collection.FindAsync(new BsonDocument()))
+            {
+                while (await cursor.MoveNextAsync())
+                {
+                    IEnumerable<BsonDocument> batch = cursor.Current;
+                    foreach (BsonDocument document in batch)
+                    {
+                        Console.WriteLine(document);
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ LiveChatServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LiveChatServer.Helpers;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace LiveChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //DBConnection.EstablishConnection();
+            Task t1 = DBConnection.EstablishConnection();
+
+            Task.WaitAll(t1);
+
+            Console.WriteLine("# Program Ends #");
+        }
+
+        
+    }
+}
